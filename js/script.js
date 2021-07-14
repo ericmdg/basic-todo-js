90511064231c9cdb0c470cbeb42e24a46f5876c4
Eric Rasolofonirina <ericseven.mg@gmail.com>
2021-07-14 20:20:52 +0300

    Improvement

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var inputElement = document.querySelector('input')
 var buttonElement = document.querySelector('button')
 var ulElement = document.querySelector('ul')
 
+var todos = []
+
 buttonElement.addEventListener('click', addTodo)
 
 function addTodo() {
@@ -9,13 +11,25 @@ function addTodo() {
     if (userInput.trim() == '') {
         return
     }
+    var newTodo = { id: Math.random(), value : userInput }
+    todos.push(newTodo)
     var todoList = document.createElement('li')
     todoList.textContent = userInput
     todoList.addEventListener('click', removeTodo)
+    todoList.dataset.id = newTodo.id
     ulElement.appendChild(todoList)
+    console.log(todos)
 }
 
 function removeTodo(event) {
     var clickedList = event.target
+    var itemId = clickedList.dataset.id
+    for (var i = 0; i < todos.length; i++) {
+        if (todos[i].id == itemId) {
+            todos.splice(i, 1)
+            break
+        }
+    }
     clickedList.parentNode.removeChild(clickedList)
+    console.log(todos)
 }
